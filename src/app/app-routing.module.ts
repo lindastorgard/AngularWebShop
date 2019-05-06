f232dffa1ef1e0a3800c5cce88cc97fb120bc016
lindastorgard <linda.storgard@gmail.com>
2019-05-06 13:17:34 +0200

    add routing for home and category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { CategoryComponent } from './category/category.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: HomeComponent },
+  { path: 'search', component: SearchComponent},
+  { path: 'category', component: CategoryComponent},
+  // details
+  // { path: 'details/:id', component: DetailsComponent },
+  { path: '', component: HomeComponent },
+  // { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
