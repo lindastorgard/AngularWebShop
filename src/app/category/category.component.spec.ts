92cf6646e7110ee5e74e17a0723c4b574ebc562d
lindastorgard <linda.storgard@gmail.com>
2019-05-13 11:10:48 +0200

    added provider in categoty.component.spec.ts and added testingdata activateRouteStubs.ts

--- src/app/category/category.component.spec.ts
+++ src/app/category/category.component.spec.ts
@@ -2,17 +2,26 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CategoryComponent } from './category.component';
 import { DataService } from '../Services/data.service';
 import { MockDataService } from '../Services/mock-data.service';
+import { ActivatedRouteStub } from './testing/activateRouteStubs';
+import { ActivatedRoute } from '@angular/router';
+
 
 describe('CategoryComponent', () => {
   let component: CategoryComponent;
   let fixture: ComponentFixture<CategoryComponent>;
+
+  const activatedRouteStub = new ActivatedRouteStub({ id: 2 });
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CategoryComponent ],
 
       // Vid test aktivera denna!
-      providers: [{provide: DataService, useClass: MockDataService}]
+      // providers: [{provide: DataService, useClass: MockDataService}],
+
+      providers: [{ provide: ActivatedRoute, useValue: activatedRouteStub },
+        { provide: DataService, useClass: MockDataService}]
     })
     .compileComponents();
   }));
@@ -26,4 +35,10 @@ describe('CategoryComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
 });
