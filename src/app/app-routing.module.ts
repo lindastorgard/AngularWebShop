d099a1ef0d9a7b6c9fcada356aa498eed7b32400
lindastorgard <linda.storgard@gmail.com>
2019-05-18 20:31:39 +0200

    add routing for alsolike component to details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { CategoryComponent } from './category/category.component';
 import { DetailsComponent } from './details/details.component';
-import { AlsolikeComponent } from './alsolike/alsolike.component';
 
 const routes: Routes = [
 
   { path: 'search', component: SearchComponent},
   { path: 'categories/:id', component: CategoryComponent },
-  { path: 'categories/:id', component: AlsolikeComponent },
   { path: 'details/:id', component: DetailsComponent },
-  // details
-  // { path: 'details/:id', component: DetailsComponent },
+  { path: 'alsolike/:id', component:DetailsComponent },
   { path: '', component: HomeComponent },
   // { path: '**', component: NotfoundComponent },
 ];
