d6e96fae931c55d9d8a10c28445cdb912a157377
lindastorgard <linda.storgard@gmail.com>
2019-05-20 11:35:15 +0200

    add footer2 component

---
+++ src/app/footer2/footer2.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../Services/data.service';
+import { ICategories } from '../Interfaces/ICategories';
+
+@Component({
+  selector: 'app-footer2',
+  templateUrl: './footer2.component.html',
+  styleUrls: ['./footer2.component.css']
+})
+export class Footer2Component implements OnInit {
+
+  categories: ICategories[];
+
+  constructor(private service: DataService) { }
+
+  ngOnInit() {
+
+    this.service.getCategories().subscribe(
+      (dataCategories) => {
+        this.categories = dataCategories;
+        // console.log(dataCategories);
+      });
+  }
+}
