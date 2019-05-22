de8ec5b77fdeaeb050c53a586f8d868725bf2314
lindastorgard <linda.storgard@gmail.com>
2019-05-22 13:22:11 +0200

    add styling and function to show objects from sessionStorage in checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IMovie } from '../Interfaces/IMovie';
 
 @Component({
   selector: 'app-checkout',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
+  myCart: IMovie[] = [];
+
   constructor() { }
 
   ngOnInit() {
+
+    if (sessionStorage) {
+      this.myCart = JSON.parse(sessionStorage.getItem("myStoredItems"));
+      console.log(this.myCart);
+    }
   }
 
 }
