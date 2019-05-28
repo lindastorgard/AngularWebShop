a6705094434aed054e24b2fe113c6811c76e9844
lindastorgard <linda.storgard@gmail.com>
2019-05-28 12:50:41 +0200

    add form and functions to set and save order

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { IMovie } from '../Interfaces/IMovie';
 import { ICart } from '../Interfaces/ICart';
-import { IOrderRow } from '../Interfaces/IOrderRows';
+import { IOrderRows } from '../Interfaces/IOrderRows';
 import * as moment from 'moment';
 import { DataService } from '../Services/data.service';
-import { FormControl, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 
 export class Constants {
@@ -21,9 +21,13 @@ export class CheckoutComponent implements OnInit {
   now = moment().format(new Constants().dateTimeFormat);
   myCart: IMovie[] = [];
   checkoutItems: ICart;
-  orderRows: IOrderRow;
+  orderRows: IOrderRows[];
   movies: IMovie[];
-  checkoutEmail = new FormControl('');
+  errorMessage = '';
+  checkoutDetails: FormGroup;
+
+
+
 
   constructor(private service: DataService, private fb: FormBuilder) { }
 
@@ -32,33 +36,64 @@ export class CheckoutComponent implements OnInit {
     if (sessionStorage) {
       this.myCart = JSON.parse(sessionStorage.getItem("myStoredItems"));
       // console.log(this.myCart);
+
+      this.service.getData().subscribe(
+        (dataMovies) => {
+          this.movies = dataMovies;
+          // console.log(this.movies);
+
+          let orderRows: IOrderRows[] = [];
+          this.myCart.forEach((cartItem) => {
+            orderRows.push({ ProductId: cartItem.id, Amount: 1 });
+          })
+
+          // console.log("E-mail:", this.email.value);
+
+
+        });
+
+      this.checkoutDetails = this.fb.group({
+        // firstname: ['', Validators.required],
+        // lastname: ['', Validators.required],
+        // email: ['', [Validators.required, Validators.email]]
+
+        firstname: [''],
+        lastname: [''],
+        email: ['']
+      });
+
+    
+    } else {
+      this.errorMessage = "Your cart is empty.";
     }
 
-    this.service.getData().subscribe(
-      (dataMovies) => {
-        this.movies = dataMovies;
-        // console.log(this.movies);
+  }
 
-        let orderRows: IOrderRow[] = [];
-        this.myCart.forEach((cartItem) => {
-          orderRows.push({ ProductId: cartItem.id, Amount: 1 });
-        })
 
-        console.log("E-mail:", this.checkoutEmail.value);
+  setOrder() {
+    this.checkoutItems = {
+      companyId: 16,
+      created: this.now,
+      createdBy: this.checkoutDetails.value.email,
+      paymentMethod: null,
+      totalPrice: 0,
+      status: 0,
+      orderRows: this.orderRows
 
-        this.checkoutItems = {
-          companyId: 16,
-          created: this.now,
-          createdBy: this.checkoutEmail.value,
-          paymentMethod: null,
-          totalPrice: 0,
-          status: 0,
-          orderRows: orderRows
-
-        }
-
-        console.log(this.checkoutItems);
-
-      });
+      // companyId: number, 
+      // created: string, 
+      // createdBy: string, 
+      // paymentMethod: string, 
+      // totalPrice: number, 
+      // status: number, 
+      // orderRows: IOrderRows[];
+    }
+    console.log("my items", this.checkoutItems);
+    
+    // this.service.createOrder(this.checkoutItems).subscribe(
+    //   response => console.log(response)
+    // );
   }
 }
+
+
