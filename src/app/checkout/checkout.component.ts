a5013793846e28d8665bdee2908a1ce9a10fb649
lindastorgard <linda.storgard@gmail.com>
2019-06-12 20:07:09 +0200

    finish checkout component, all tests passed

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -4,7 +4,9 @@ import { ICart } from '../Interfaces/ICart';
 import { IOrderRows } from '../Interfaces/IOrderRows';
 import * as moment from 'moment';
 import { DataService } from '../Services/data.service';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from '../Services/message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
@@ -21,40 +23,60 @@ export class CheckoutComponent implements OnInit {
   formDetails: FormGroup;
   totalAmount: number;
   orderRow: IOrderRows[];
+  deleted: boolean;
+  isCartActive: boolean = true;
+  cartMessage: string;
+  cartItem: IMovie;
 
-  constructor(private service: DataService, private fb: FormBuilder) { }
+  constructor(private service: DataService, private fb: FormBuilder, private messageService: MessageService, private router: Router) { }
 
   ngOnInit() {
 
-    console.log(this.now);
-
     if (sessionStorage) {
-      this.myCart = JSON.parse(sessionStorage.getItem("myStoredItems"));
-      console.log("myCart", this.myCart);
+      this.myCart = JSON.parse(sessionStorage.getItem("myStoredItems")) || [];
+      this.toggleVisibility();
 
       this.service.getData().subscribe(
         (dataMovies) => {
           this.movies = dataMovies;
-
           this.orderRow = [];
-          this.myCart.forEach((cartItem) => {
-            this.orderRow.push({ ProductId: cartItem.id, Amount: 1 });
-          })
+
+          if (this.myCart) {
+            this.myCart.forEach((cartItem) => {
+              this.orderRow.push({ productId: cartItem.id, amount: 1 });
+            })
+          }
+
         });
 
-      this.totalAmount = 0;
-      for (var i = 0; i < this.myCart.length; i++) {
-        var item = this.myCart[i];
-        this.totalAmount += item.price;
-      }
+      this.totalOrderAmount();
 
+      let emailRegex: any = '^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$';
       this.formDetails = this.fb.group({
-        firstname: ['', Validators.required],
-        lastname: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]]
+        'firstname': ['', Validators.required],
+        'lastname': ['', Validators.required],
+        'email': ['', [Validators.required, Validators.email, Validators.pattern(emailRegex)]]
       });
 
     }
+  }
+
+  deleteCartItem(id: number) {
+    for (let i = 0; i < this.myCart.length; i++) {
+      if (this.myCart[i].id === id) {
+        this.myCart.splice(i, 1);
+      }
+    }
+    this.toggleVisibility();
+
+    if (this.myCart.length == 0 || this.myCart == null) {
+      sessionStorage.clear();
+    } else {
+      sessionStorage.setItem("myStoredItems", JSON.stringify(this.myCart));
+    }
+
+    this.totalOrderAmount();
+    this.messageService.setDeletedMessage(true);
   }
 
   setOrder() {
@@ -67,13 +89,35 @@ export class CheckoutComponent implements OnInit {
       status: 0,
       orderRows: this.orderRow
     };
-    console.log("my items", this.checkoutItems);
-    console.log(this.orderRow);
-
+ 
     this.service.createOrder(this.checkoutItems).subscribe(
-      response => console.log(response)
+      (response) => console.log(response)
     );
 
+    this.formDetails.reset();
     sessionStorage.clear();
+    this.messageService.setDeletedMessage(true);
+
+    setTimeout(() => {
+      this.router.navigate(['thankyou']);
+    }, 1500);
   }
+
+  totalOrderAmount() {
+    this.totalAmount = 0;
+    for (var i = 0; i < this.myCart.length; i++) {
+      this.cartItem = this.myCart[i];
+      this.totalAmount += this.cartItem.price;
+    }
+  }
+
+  toggleVisibility() {
+    if (!sessionStorage || this.myCart == null || this.myCart.length == 0) {
+      this.isCartActive = true;
+      this.cartMessage = "Your cart is empty. Start browsing your favourite movies now by selecting a category.";
+    } else {
+      this.isCartActive = false;
+    }
+  }
+
 }
